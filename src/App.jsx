cdbe29fc595d900751dbcc9c22fba96be6a709b7
Dodong2 <carlseighartaliodegmail.com>
2024-12-14 21:20:21 +0800

    3nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import {db} from './firebase-config.js'
-import {collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore'
+import {collection, getDocs, addDoc, doc, deleteDoc, orderBy, query,serverTimestamp} from 'firebase/firestore'
 import UpdateUser from './UpdateUser.jsx'
 import './App.css'
 
@@ -14,25 +14,24 @@ const usersCollectionRef = collection(db, "users")
 
 //logics
 //pang Create ng data sa firebase
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) })
-  }
+const createUser = async () => {
+  await addDoc(usersCollectionRef, {
+    name: newName,
+    age: Number(newAge),
+    createdAt: serverTimestamp(), // Add this line
+  });
+};
 //pang get ng data sa Firebase
   useEffect(() => {
     const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef)
-      console.log(data)
+      const q = query(usersCollectionRef, orderBy("createdAt", "desc"))
+      const data = await getDocs(q)
+      // console.log(data)
       setUsers(data.docs.map((doc) =>({ ...doc.data(), id: doc.id })))
     }
     getUsers()
   }, [])
 
-// pang update ng mga data's
-  // const updateUser = async (id, age) => {
-  //   const userDoc = doc(db, "users", id)
-  //   const newFields = {age: age + 1}
-  //   await updateDoc(userDoc, newFields)
-  // }
 //pang delete
   const deleteUser = async (id) => {
     const userDoc = doc(db, "users", id)
@@ -50,14 +49,14 @@ const usersCollectionRef = collection(db, "users")
     </div>
 {/* see Data's */}
     <div className='see data'>
-        {users.map((user) => {
-          return <><div><h1>Name: {user.name} </h1></div>
+        {users.map((user) => <>
+          <div key={user.id}><h1>Name: {user.name} </h1></div>
            <div><h1>Age: {user.age} </h1></div>
            <button onClick={() => setSelectUser(user)}>Update</button>
            <button onClick={() => {deleteUser(user.id)}} >Delete</button>
            </>
-        })}
-           </div>
+        )}
+        </div>
     </div>
 
      {/* Show UpdateUser Component */}
